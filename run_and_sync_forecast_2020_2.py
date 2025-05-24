e033495b0ef4549e03e94cd5d88da21774a40f78
anpetry-admin <anpetry@htwsaar.de>
2025-05-24 20:53:29 +0200

    no message

--- run_and_sync_forecast_2020_2.py
+++ run_and_sync_forecast_2020_2.py
@@ -131,7 +131,7 @@ def crop_and_prepare_and_upload(local_grib_path):
     print(f"[UPLOAD] Upload complete: {file_name}")
 
 def run_forecasts():
-    start_date = datetime(2020, 10, 21)
+    start_date = datetime(2020, 11, 7)
     end_date = datetime(2020, 12, 31)
     lead_time = 168
     time_str = "1200"
